ed1b6f74346e0484c3bc0fb8ac8fda245a2278a7
Plinio Balduino <pbalduino+github@gmail.com>
2011-10-22 16:24:04 -0200

    Version bump to 0.1.1

--- spec/cetsp_spec.rb
+++ spec/cetsp_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Cetsp" do
 
   SOURCE = File.expand_path(File.dirname(__FILE__) + '/../spec/sample/ocorrencias.html')
+  EMPTY  = File.expand_path(File.dirname(__FILE__) + '/../spec/sample/empty.html')
 
   before :each do
     @cetsp = CetSp::Parser.new(SOURCE)
@@ -13,7 +14,17 @@ describe "Cetsp" do
   end
 
   it "should return whatever" do
-    p '---->', @cetsp.parse
+    @cetsp.parse.first.code.should == 206
+  end
+
+  it "should read an empty resource and return an empty array" do
+    @cetsp = CetSp::Parser.new(EMPTY)
+    @cetsp.parse.should == []
+
+  end
+
+  it "should work with real site" do
+    p CetSp::Parser.new("http://cetsp1.cetsp.com.br/monitransmapa/IMG10/ocorrencias.asp").parse
   end
 
 end
