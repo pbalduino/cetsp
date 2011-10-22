aa0faf2f1d4da87de54453feb9eabe5f86afc185
Plinio Balduino <pbalduino+github@gmail.com>
2011-10-22 16:24:22 -0200

    meh

--- lib/cetsp.rb
+++ lib/cetsp.rb
@@ -1,25 +1,23 @@
 # encoding UTF-8
 
 require 'nokogiri'
+require 'open-uri'
 
 module CetSp
   class Parser
 
     def initialize(source)
-      @parser = Nokogiri::HTML(open(source), nil, 'UTF-8')
+      puts source
+      @parser = Nokogiri::HTML(open(source), nil, 'ASCII-8BIT')
       @events = []
     end
 
     def parse
-      rows = @parser.xpath('//body/center/table[2]/tr')
+      rows = @parser.search('tr.cor1', 'tr.cor2')
       rows.each do |row|
-        begin
-          @events << Event.new(:code => row.children[0].to_str.gsub(/\302\240/, ' ').strip.to_i,
-                               :place => row.children[2].to_str.gsub(/\302\240/, ' ').strip,
-                               :direction => row.children[4].to_str.gsub(/\302\240/, ' ').strip,
-                               :since => DateTime.strptime(row.children[6].to_str.gsub(/\302\240/, ' ').strip, "%d/%m/%y-%H:%M"))
-        rescue
-        end
+        @events << Event.new(:code =>      row.children[0].to_str.force_encoding("ASCII-8BIT").gsub(/\302\240/, ' ').strip.to_i,
+                             :place =>     row.children[2].to_str.force_encoding("ASCII-8BIT").gsub(/\302\240/, ' ').strip,
+                             :direction => row.children[4].to_str.force_encoding("ASCII-8BIT").gsub(/\302\240/, ' ').strip)
       end
       @events
     end
@@ -32,7 +30,6 @@ module CetSp
       @code = options[:code]
       @place = options[:place]
       @direction = options[:direction]
-      @since = options[:since]
     end
   end
 end
